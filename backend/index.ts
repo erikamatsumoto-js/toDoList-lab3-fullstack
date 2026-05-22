103330b227499c824b85fc801b79c1f709a3e69c
Erika Matsumoto <matmoterika816@gmail.com>
2026-05-22 11:28:27 +0200

    delete frontend map, create new frontend map and then installed react-ts.fixed cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,28 @@
 import express from "express";
+import * as sqlite from "sqlite";
+import type { Database } from "sqlite";
+import sqlite3 from "sqlite3";
+
+export let database: Database;
 
 const app = express();
 
+database = await sqlite.open({
+  driver: sqlite3.Database,
+  filename: "lab3.sqlite",
+});
+
+await database.run("PRAGMA foreign_keys = ON");
+
+//måste placeras ovanför alla app.get,put,delete anroper (den ska köras först)
+app.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.get("/", (_request, response) => {
-  response.send("Hello World!");
+  response.send({ msg: "Hello World!" });
 });
 
 app.listen(3000, () => {
